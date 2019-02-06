656c7c56d476f7986f584ec68a829acc421fc82b
Samuel Cornet <samuel@noukod.com>
2019-02-06 19:39:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(endYear){
+   return endYear - this.startDate.getFullYear() -1;
+  }
+}
